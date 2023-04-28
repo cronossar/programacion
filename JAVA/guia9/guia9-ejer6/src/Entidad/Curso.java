81128971b302071c11c3c59234f202f2d2afbe13
cronossar <cronossar@gmail.com>
2023-04-28 11:23:09 -0300

    actualizacion mañana

--- JAVA/guia9/guia9-ejer6/src/Entidad/Curso.java
+++ JAVA/guia9/guia9-ejer6/src/Entidad/Curso.java
@@ -5,20 +5,23 @@ import java.util.Arrays;
 public class Curso {
     
     private String nombreCurso;
-    private Integer cantidadHorasPorDia;
-    private Integer cantidadDiasPorSemana;
-    private char turno;
+    private int cantidadHorasPorDia;
+    private int cantidadDiasPorSemana;
+    private String turno;
     private double precioPorHora;
+    private String[] alumnos;
 
     public Curso() {
+        // Constructor por defecto
     }
 
-    public Curso(String nombreCurso, Integer cantidadHorasPorDia, Integer cantidadDiasPorSemana, char turno, double precioPorHora) {
+    public Curso(String nombreCurso, int cantidadHorasPorDia, int cantidadDiasPorSemana, String turno, double precioPorHora, String[] alumnos) {
         this.nombreCurso = nombreCurso;
         this.cantidadHorasPorDia = cantidadHorasPorDia;
         this.cantidadDiasPorSemana = cantidadDiasPorSemana;
         this.turno = turno;
         this.precioPorHora = precioPorHora;
+        this.alumnos = alumnos;
     }
 
     public String getNombreCurso() {
@@ -29,27 +32,27 @@ public class Curso {
         this.nombreCurso = nombreCurso;
     }
 
-    public Integer getCantidadHorasPorDia() {
+    public int getCantidadHorasPorDia() {
         return cantidadHorasPorDia;
     }
 
-    public void setCantidadHorasPorDia(Integer cantidadHorasPorDia) {
+    public void setCantidadHorasPorDia(int cantidadHorasPorDia) {
         this.cantidadHorasPorDia = cantidadHorasPorDia;
     }
 
-    public Integer getCantidadDiasPorSemana() {
+    public int getCantidadDiasPorSemana() {
         return cantidadDiasPorSemana;
     }
 
-    public void setCantidadDiasPorSemana(Integer cantidadDiasPorSemana) {
+    public void setCantidadDiasPorSemana(int cantidadDiasPorSemana) {
         this.cantidadDiasPorSemana = cantidadDiasPorSemana;
     }
 
-    public char getTurno() {
+    public String getTurno() {
         return turno;
     }
 
-    public void setTurno(char turno) {
+    public void setTurno(String turno) {
         this.turno = turno;
     }
 
@@ -60,6 +63,24 @@ public class Curso {
     public void setPrecioPorHora(double precioPorHora) {
         this.precioPorHora = precioPorHora;
     }
-    
-    
+
+    public String[] getAlumnos() {
+        return alumnos;
+    }
+
+    public void setAlumnos(String[] alumnos) {
+        this.alumnos = alumnos;
+    }
+
+    public void cargarAlumnos() {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Ingrese el nombre del alumno " + (i+1) + ": ");
+            this.alumnos[i] = scanner.nextLine();
+        }
+    }
+
+    public double calcularGananciaSemanal() {
+        return this.cantidadHorasPorDia * this.precioPorHora * this.alumnos.length * this.cantidadDiasPorSemana;
+    }
 }
--- JAVA/guia9/guia9-ejer6/src/Servicio/CursoServicio.java
+++ JAVA/guia9/guia9-ejer6/src/Servicio/CursoServicio.java
@@ -1,122 +1,53 @@
 package Servicio;
 
 import java.util.Scanner;
-import Entidad.Curso;
+
 
 public class CursoServicio {
-      
-    private Scanner scanner;
-
-    public CursoServicio() {
-        this.scanner = new Scanner(System.in);
-    }
     
-    public void cargarAlumnos(){
-        String nombre;
-        for (int i = 0; i < alumnos.length; i++) {
-            System.out.println("Ingresa el nombre del alumno");
-            nombre = scanner.next();
-            alumnos[i] = nombre;            
-        }
-        for (int i : alumnos) {
-            System.out.println(i+" "); 
+    public void cargarAlumnos() {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Ingrese el nombre del alumno " + (i + 1) + ": ");
+            alumnos[i] = scanner.nextLine();
         }
     }
+
+    public void crearCurso() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el nombre del curso: ");
+        nombreCurso = scanner.nextLine();
+
+        System.out.print("Ingrese la cantidad de horas por día: ");
+        cantidadHorasPorDia = scanner.nextInt();
+
+        System.out.print("Ingrese la cantidad de días por semana: ");
+        cantidadDiasPorSemana = scanner.nextInt();
+
+        System.out.print("Ingrese el turno (mañana o tarde): ");
+        turno = scanner.next();
+
+        System.out.print("Ingrese el precio por hora: ");
+        precioPorHora = scanner.nextDouble();
+
+        alumnos = new String[5];
+        cargarAlumnos();
+    }
+
+    public double calcularGananciaSemanal() {
+        return cantidadHorasPorDia * precioPorHora * 5 * cantidadDiasPorSemana * alumnos.length;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "nombreCurso='" + nombreCurso + '\'' +
+                ", cantidadHorasPorDia=" + cantidadHorasPorDia +
+                ", cantidadDiasPorSemana=" + cantidadDiasPorSemana +
+                ", turno='" + turno + '\'' +
+                ", precioPorHora=" + precioPorHora +
+                ", alumnos="
+
     
-}
-   
-//-------------------------------------------------------
-
-public Curso crearCurso() {
-            /*
-            this.nombreCurso = nombreCurso;
-            this.cantidadHorasPorDia = cantidadHorasPorDia;
-            this.cantidadDiasPorSemana = cantidadDiasPorSemana;
-            this.turno = turno;
-            this.precioPorHora = precioPorHora;
-Curso(String nombreCurso, String cantidadHorasPorDia, String cantidadDiasPorSemana, String turno, double precioPorHora)             
-             */
-            System.out.println("ingrese nombre curso, cantidad horas dia, semana, turno, precio por hora y cargue los alumnos");
-            Curso c = new Curso(leer.nextLine(),leer.nextLine(),leer.nextLine(),leer.nextLine(),leer.nextDouble()); 
-            c.setAlumnos(cargarAlumnos());
-            return c;
-        }
-        
-        public void mostrar(Curso c){
-            
-            System.out.println(c.getAlumnos());
-            System.out.println(c.getCantidadDiasPorSemana());
-            System.out.println(c.getCantidadHorasPorDia());
-            System.out.println(c.getNombreCurso());
-            System.out.println(c.getPrecioPorHora());
-            System.out.println(c.getTurno());
-        }
-
     }
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Servicios;
-
-import Entidad.Curso;
-import java.util.Scanner;
-
-/**
- *
- * @author bruno
- */
-public class CrusoServicios {
-
-
-        Scanner leer = new Scanner(System.in);
-        Curso c = new Curso();
-        public String[] cargarAlumnos() {
-            String[] alumno = new String[5];
-            for (int i = 0; i < 5; i++) {
-                System.out.println("ingres el nombre del alumno");
-                alumno[i] = leer.nextLine();
-            }
-            return alumno;
-        }
-
-        public Curso crearCurso() {
-            /*
-            this.nombreCurso = nombreCurso;
-            this.cantidadHorasPorDia = cantidadHorasPorDia;
-            this.cantidadDiasPorSemana = cantidadDiasPorSemana;
-            this.turno = turno;
-            this.precioPorHora = precioPorHora;
-Curso(String nombreCurso, String cantidadHorasPorDia, String cantidadDiasPorSemana, String turno, double precioPorHora)             
-             */
-            System.out.println("ingrese nombre curso, cantidad horas dia, semana, turno, precio por hora y cargue los alumnos");
-            Curso c = new Curso(leer.nextLine(),leer.nextLine(),leer.nextLine(),leer.nextLine(),leer.nextDouble()); 
-            c.setAlumnos(cargarAlumnos());
-            return c;
-        }
-        
-        public void mostrar(Curso c){
-            
-            System.out.println(c.getAlumnos());
-            System.out.println(c.getCantidadDiasPorSemana());
-            System.out.println(c.getCantidadHorasPorDia());
-            System.out.println(c.getNombreCurso());
-            System.out.println(c.getPrecioPorHora());
-            System.out.println(c.getTurno());
-        }
-
-    }
-
-
-/*
--Método cargarAlumnos(): este método le permitirá al usuario ingresar los alumnos 
-que asisten a las clases. Nosotros nos encargaremos de almacenar esta información 
-en un arreglo e iterar con un bucle, solicitando en cada repetición que se ingrese el nombre de cada alumno.
--Método crearCurso(): el método crear curso, le pide los valores de los atributos 
-al usuario y después se le asignan a sus respectivos atributos para llenar el objeto Curso. 
-En este método invocamos al método cargarAlumnos() para asignarle valor al atributo alumnos
--Método calcularGananciaSemanal(): este método se encarga de calcular la ganancia en 
-una semana por curso. Para ello, se deben multiplicar la cantidad de horas por día, el precio por hora, 
-la cantidad de alumnos y la cantidad de días a la semana que se repite el encuentro.
- */ 
-
