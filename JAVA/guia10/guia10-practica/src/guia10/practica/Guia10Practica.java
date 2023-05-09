fc259c0898cfcb667c982701cb3669bdbf95cf54
cronossar <127250505+cronossar@users.noreply.github.com>
2023-05-09 20:47:51 -0300

    colecciones martes

--- JAVA/guia10/guia10-practica/src/guia10/practica/Guia10Practica.java
+++ JAVA/guia10/guia10-practica/src/guia10/practica/Guia10Practica.java
@@ -8,25 +8,34 @@ import java.util.HashSet;
 public class Guia10Practica {
 
     public static void main(String[] args) {
+        
         // LISTAS
         ArrayList<Integer> numerosA = new ArrayList();
         int x = 20;
         int z = 10;
         int c = 25;
         int v = 33;
-        int n = 45;
+        int n = 25;
         numerosA.add(x);
         numerosA.add(z);
         numerosA.add(c);
         numerosA.add(v);
         numerosA.add(n);
         System.out.println("ARRAYLIST - Fueron cargados");
-        System.out.println(numerosA);
+        int cont = 0;
+        for(Integer cadena : numerosA){
+            cont++;
+//            numerosA.remove(Integer.valueOf(33));//borra el objeto
+        System.out.print("{ "+cont+" } "+cadena+ " , ");
+        }
+        System.out.println("");
+        System.out.println("#############");
         numerosA.remove(1);//borra el dato del subindice 1
         System.out.println("Fue removido el dato del indice 1");
         System.out.println(numerosA);
-//        numerosA.remove(33);//borra el objeto
-//        System.out.println(numerosA);
+        System.out.println("Removemos el objeto o valor");
+        numerosA.remove(Integer.valueOf(45));//borra el objeto
+        System.out.println(numerosA);
         
         //CONJUNTOS
         HashSet<Integer> numerosB = new HashSet();
@@ -34,7 +43,7 @@ public class Guia10Practica {
         Integer q = 45;
         Integer w = 789;
         Integer r = 124;
-        Integer o = 987;
+        Integer o = 45;
         
         numerosB.add(y);
         numerosB.add(q);
