1699a975ce189c8b84c8334aa9ad7844a21214df
cronossar <127250505+cronossar@users.noreply.github.com>
2023-05-08 21:42:39 -0300

    arrancamos guia10 colecciones

---
+++ JAVA/guia10/guia10-practica/src/guia10/practica/Guia10Practica.java
@@ -0,0 +1,76 @@
+
+package guia10.practica;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Guia10Practica {
+
+    public static void main(String[] args) {
+        // LISTAS
+        ArrayList<Integer> numerosA = new ArrayList();
+        int x = 20;
+        int z = 10;
+        int c = 25;
+        int v = 33;
+        int n = 45;
+        numerosA.add(x);
+        numerosA.add(z);
+        numerosA.add(c);
+        numerosA.add(v);
+        numerosA.add(n);
+        System.out.println("ARRAYLIST - Fueron cargados");
+        System.out.println(numerosA);
+        numerosA.remove(1);//borra el dato del subindice 1
+        System.out.println("Fue removido el dato del indice 1");
+        System.out.println(numerosA);
+//        numerosA.remove(33);//borra el objeto
+//        System.out.println(numerosA);
+        
+        //CONJUNTOS
+        HashSet<Integer> numerosB = new HashSet();
+        Integer y = 30;
+        Integer q = 45;
+        Integer w = 789;
+        Integer r = 124;
+        Integer o = 987;
+        
+        numerosB.add(y);
+        numerosB.add(q);
+        numerosB.add(w);
+        numerosB.add(r);
+        numerosB.add(o);
+        System.out.println("CONJUNTOS - Fueron cargados");
+        System.out.println(numerosB);
+        numerosB.remove(45);
+        System.out.println("Fue removido el objeto 45");
+        System.out.println(numerosB);
+        
+        //MAPAS
+        HashMap<Integer, String> alumnos = new HashMap();
+        
+        int dni = 21302230;
+        String nombre = "Daniel";
+        
+        int dni1 = 23458788;
+        String nombre1 = "Juana";
+        
+        int dni2 = 45786233;
+        String nombre2 = "Hugo";
+        
+        int dni3 = 32458652;
+        String nombre3 = "Laura";
+        
+        alumnos.put(dni, nombre);
+        alumnos.put(dni1, nombre1);
+        alumnos.put(dni2, nombre2);
+        alumnos.put(dni3, nombre3);
+        System.out.println("MAPAS - Fueron cargados");
+        System.out.println(alumnos);
+        System.out.println("Fue removido el dni2 de Hugo");
+        alumnos.remove(45786233);
+        System.out.println(alumnos);
+    }
+    
+}
