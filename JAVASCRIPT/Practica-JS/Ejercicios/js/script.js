e96a9af95720da8be446acb8d1ed437ae8d29864
cronossar <127250505+cronossar@users.noreply.github.com>
2023-08-14 17:20:31 -0300

    ponemos_al_dia

---
+++ JAVASCRIPT/Practica-JS/Ejercicios/js/script.js
@@ -0,0 +1,38 @@
+/*ejercicio 1
+function saludo1(){
+    var tiempo = prompt("¿Cómo esta el día de hoy?")
+    alert("El día está " + tiempo);
+};
+//ejercicio 2
+function calc_area(){
+    var radio = prompt("Ingresa un valor para el radio de la circunferencia");
+    var pi = 3.141516;
+    let area = pi * (radio * radio);
+    alert ("Para el radio dado, el área del circulo es :" + area);
+
+    alert("Con el radio dado, calculare el perímetro");
+    let peri = (2*pi) * radio;
+    alert ("Para el radio dado, el perímetro del circulo es :" + peri);
+}
+//ejercicio 3
+
+
+//ejercicio4
+function veriletra(){
+    var letra = prompt("Ingrese una letra, pero solo puede ser : S ó N);
+    var veri = "S" 
+}
+var letra = */
+
+switch (expr) {
+    case 'Naranjas':
+      console.log('El kilogramo de naranjas cuesta $0.59.');
+      break;
+    case 'Mangos':
+    case 'Papayas':
+      console.log('El kilogramo de mangos y papayas cuesta $2.79.');
+      break;
+    default:
+      console.log('Lo lamentamos, por el momento no disponemos de ' + expr +
+  '.');
+  } 
