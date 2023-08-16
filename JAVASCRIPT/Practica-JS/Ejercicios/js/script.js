24136cfba0dce6c8b46aed337acd4650cfc15294
cronossar <127250505+cronossar@users.noreply.github.com>
2023-08-15 21:36:16 -0300

    noche

--- JAVASCRIPT/Practica-JS/Ejercicios/js/script.js
+++ JAVASCRIPT/Practica-JS/Ejercicios/js/script.js
@@ -26,11 +26,29 @@ function calc_area(){
 }*/
 
 //ejercicio4
+/*Realiza un programa que sólo permita introducir los caracteres ‘S’ y ‘N’. Si el usuario
+ingresa alguno de esos dos caracteres se deberá de imprimir un mensaje por pantalla
+que diga “CORRECTO”, en caso contrario, se deberá imprimir “INCORRECTO”. */
+
 /*function veriletra(){
-    var letra = prompt("Ingrese una letra, pero solo puede ser : S ó N);
-    var veri = "S";
-}
-var letra = */
+    var letra = prompt("Ingrese una letra, pero solo puede ser : S ó N");
+    letra = letra.toUpperCase();
+    if (letra == "S" || letra == "N" ) {
+      alert("CORRECTO");
+    } else {
+      alert("INCORRECTO");
+      
+    }
+}*/
+
+//ejercicio5
+/*Construir un programa que simule un menú de opciones para realizar las cuatro
+operaciones aritméticas básicas (suma, resta, multiplicación y división) con dos valores
+numéricos enteros. El usuario, además, debe especificar la operación con el primer
+carácter de la operación que desea realizar: ‘S' o ‘s’ para la suma, ‘R’ o ‘r’ para la resta, ‘M’
+o ‘m’ para la multiplicación y ‘D’ o ‘d’ para la división*/
+
+
 
 
 //ejercicio6
@@ -52,7 +70,7 @@ En caso de que el valor ingresado sea 0, se debe mostrar “el número no es par
 /*Escriba un programa en el cual se ingrese un valor límite positivo, y a continuación
 solicite números al usuario hasta que la suma de los números introducidos supere el
 límite inicial*/
-function num_limite(){
+/*function num_limite(){
   var limite = parseInt(prompt("Ingrese un numero entero positivo de REFERENCIA"));
   var cont = 0;
   do {
@@ -61,10 +79,67 @@ function num_limite(){
   } while (cont <= limite);
 
   alert("Yata")
+}*/
+
+//ejercicio8
+/*Escribir un programa que lea números enteros hasta teclear 0 (cero). Al finalizar el
+programa se debe mostrar el máximo número ingresado, el mínimo, y el promedio de
+todos ellos*/
+/*Este programa primero inicializa variables para rastrear el máximo,
+mínimo, suma total y la cantidad de números ingresados. Luego, entra
+en un bucle donde se lee un número entero del usuario en cada iteración.
+Si el número es 0, el bucle se rompe. De lo contrario, se actualizan las
+variables de máximo, mínimo, suma y cantidad. Finalmente, después de que
+el bucle se complete, se calcula el promedio y se muestran los resultados.
+// Inicializar variables para el máximo, mínimo y la suma total*/
+
+
+/*let maximo = Number.MIN_SAFE_INTEGER;
+let minimo = Number.MAX_SAFE_INTEGER;
+let suma = 0;
+let cantidadNumeros = 0;
+
+// Función para leer un número entero desde el usuario
+function leerNumero() {
+  return parseInt(prompt("Ingresa un número entero (ingresa 0 para finalizar):"));
 }
 
-  
+// Leer números hasta que se ingrese 0
+while (true) {
+  const numero = leerNumero();
 
+  if (numero === 0) {
+    break;
+  }
+
+  // Actualizar máximo y mínimo
+  maximo = Math.max(maximo, numero);
+  minimo = Math.min(minimo, numero);
+
+  // Actualizar la suma y cantidad de números
+  suma += numero;
+  cantidadNumeros++;
+}
+
+// Calcular el promedio
+const promedio = suma / cantidadNumeros;
+
+// Mostrar resultados
+alert("Máximo:"+ maximo);
+alert("Mínimo:"+ minimo);
+alert("Promedio:"+ promedio);
+//################################################################# */ 
+
+//ejercicio9
+/*Realizar un programa que pida una frase y el programa deberá mostrar la frase con un
+espacio entre cada letra. La frase se mostrara así: H o l a. Nota: recordar el
+funcionamiento de la función Substring().*/
+
+
+
+
+//ejercicio10
+/*Escribir una función flecha que reciba una palabra y la devuelva al revés. */
 
 
 
